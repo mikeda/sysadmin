f3c75c525879d1c475eb7d65a2e1ead7805f5b57
mikeda <baguzy@gmail.com>
2015-01-10 17:27:48 +0900

    httpdのcookbook追加

---
+++ chef/site-cookbooks/httpd/recipes/default.rb
@@ -0,0 +1,23 @@
+package 'httpd24'
+
+directory '/var/log/httpd/' do
+  mode 0755
+end
+
+%w(userdir.conf  welcome.conf).each do |conf|
+  file "/etc/httpd/conf.d/#{conf}" do
+    action :delete
+  end
+end
+
+service 'httpd' do
+  supports restart: true, reload: true
+  action [ :enable, :start ]
+end
+
+cookbook_file '/etc/httpd/conf/httpd.conf' do
+  owner  'root'
+  group  'root'
+  mode   0644
+  notifies :restart, resources(service: 'httpd')
+end
