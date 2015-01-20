6fa95c07baa5ef998064e13812eb262e2512854b
mikeda <baguzy@gmail.com>
2015-01-21 00:41:12 +0900

    bugfix

--- chef/site-cookbooks/os_setting/recipes/hostname.rb
+++ chef/site-cookbooks/os_setting/recipes/hostname.rb
@@ -1,15 +1,17 @@
-hostname = node['automatic']['ipaddress']
+fqdn = node['automatic']['ipaddress']
+
+ohai 'reload_hostname' do
+  plugin 'hostname'
+  action :nothing
+end
 
 bash 'set hostname' do
   code <<-EOC
-    hoostname #{hostname}
-    hostnamectl set-hostname #{hostname}
+    hostname #{fqdn}
+    hostnamectl set-hostname #{fqdn}
     test -f /etc/cloud/cloud.cfg && sed -i '/update_hostname/s/^/#/' /etc/cloud/cloud.cfg
     systemctl restart rsyslog
   EOC
-  only_if { node['hostname'] != hostname }
-end
-
-ohai 'reload' do
-  plugin 'hostname'
+  only_if { node['fqdn'] != fqdn }
+  notifies :reload, "ohai[reload_hostname]", :immediately
 end
