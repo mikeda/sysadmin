fc99e9b800c499e7359eed3ce27a5f22a19cdb7b
mikeda <baguzy@gmail.com>
2015-01-27 13:12:49 +0900

    set timezone

--- chef/site-cookbooks/os_setting/recipes/default.rb
+++ chef/site-cookbooks/os_setting/recipes/default.rb
@@ -1,2 +1,3 @@
 include_recipe "os_setting::hostname"
 include_recipe "os_setting::limits"
+include_recipe "os_setting::timezone"
---
+++ chef/site-cookbooks/os_setting/recipes/timezone.rb
@@ -0,0 +1,3 @@
+link '/etc/localtime' do
+  to '/usr/share/zoneinfo/Asia/Tokyo'
+end
